a52a7ebae383156dcb92c9f108590af5911326af
FabrizioFornone <fabrizio.fornone@hotmail.it>
2022-02-25 14:23:03 +0100

    Repo created

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+
+/*
+ create un file index.php in cui:
+- è definita una classe ‘Movie’
+   => all’interno della classe sono dichiarate delle variabili d’istanza
+   => all’interno della classe è definito un costruttore
+   => all’interno della classe è definito almeno un metodo
+- vengono istanziati almeno due oggetti ‘Movie’ e stampati a schermo i valori delle relative proprietà
+
+*/
+
+
+class Movie
+{
+}
